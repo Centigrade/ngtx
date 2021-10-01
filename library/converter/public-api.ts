d15356de95189286981901e86a691af4160b2db2
Julian Lang <julian.lang@centigrade.de>
2021-10-01 15:05:33 +0200

    feat: expose useful library members to outside world

---
+++ library/converter/public-api.ts
@@ -0,0 +1,4 @@
+export * from './map-result';
+export * from './string-converter/as-bool';
+export * from './string-converter/as-number';
+export * from './to-native-element';
--- library/index.ts
+++ library/index.ts
@@ -1,5 +1,7 @@
 import { tryInitChalk } from './utility';
 
+export * from './converter/public-api';
 export * from './ngtx';
+export * from './types/public-api';
 
 tryInitChalk();
