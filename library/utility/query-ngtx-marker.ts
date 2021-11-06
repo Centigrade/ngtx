6aac476dcd6494184598d9d76872f8e76e6bfbca
Julian Lang <julian.lang@hotmail.de>
2021-11-06 12:02:32 +0100

    fix: remove ngtx query marker before excuting query

--- library/utility/query-ngtx-marker.ts
+++ library/utility/query-ngtx-marker.ts
@@ -6,12 +6,22 @@ export function queryNgtxMarker<Html extends Element, Component>(
   query: string,
   debugElement: DebugElement,
 ): TypedDebugElement<Html, Component> {
-  return debugElement.query(By.css(`[data-ngtx="${query}"]`));
+  const queryTarget = stripNgtxMarker(query);
+  return debugElement.query(By.css(`[data-ngtx="${queryTarget}"]`));
 }
 
 export function queryAllNgtxMarker<Html extends Element, Component>(
   query: string,
   debugElement: DebugElement,
 ): TypedDebugElement<Html, Component>[] {
-  return debugElement.queryAll(By.css(`[data-ngtx="${query}"]`));
+  const queryTarget = stripNgtxMarker(query);
+  return debugElement.queryAll(By.css(`[data-ngtx="${queryTarget}"]`));
+}
+
+// -----------------------------------
+//  Module Internals
+// -----------------------------------
+
+function stripNgtxMarker(query: string): string {
+  return query.replace('ngtx_', '');
 }
