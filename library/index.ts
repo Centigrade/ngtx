7467c092cd46cc30d4875c6905c0e69dcc1d9397
julian.lang <julian.lang@centigrade.de>
2022-06-07 12:18:11 +0200

    fix: export set default spy factory

--- library/index.ts
+++ library/index.ts
@@ -1,4 +1,5 @@
 export * from './core/public-api';
 export * from './declarative-testing/public-api';
+export { setDefaultSpyFactory } from './global-config';
 export * from './ngtx';
 export * from './types/public-api';
