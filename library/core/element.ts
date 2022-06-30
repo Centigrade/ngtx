9930f73b6bc4dd2f3434094469e001466a6824e0
julian.lang <julian.lang@centigrade.de>
2022-06-30 16:54:38 +0200

    feat: add "read" api to elements

--- library/core/element.ts
+++ library/core/element.ts
@@ -1,8 +1,10 @@
 import { Type } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Token } from '../declarative-testing/types';
 import { ConverterFn, QueryTarget, TypedDebugElement } from '../types';
 import { isNgtxQuerySelector, printHtml, queryAll } from '../utility';
 import { removeDuplicates } from '../utility/filter.utilities';
+import { OverridableDebugElement } from '../utility/overridable-debug-element';
 import { queryNgtxMarker } from '../utility/query-ngtx-marker';
 import { NgtxMultiElement } from './multi-element';
 
@@ -130,7 +132,18 @@ export class NgtxElement<
     return trim ? text.trim() : text;
   }
 
-  debug(): void {
+  public read<Type>(type: Token<Type>): NgtxElement<Html, Type> {
+    const instance = this.injector.get(type);
+
+    const debugElement = new OverridableDebugElement<Html, Type>(
+      this.debugElement.nativeNode,
+      instance,
+    );
+
+    return new NgtxElement(debugElement);
+  }
+
+  public debug(): void {
     console.log(printHtml(this.debugElement.nativeElement));
   }
 }
--- library/core/multi-element.ts
+++ library/core/multi-element.ts
@@ -1,4 +1,5 @@
 import { Type } from '@angular/core';
+import { Token } from '../declarative-testing/types';
 import { ConverterFn, QueryTarget } from '../types';
 import { NgtxElement } from './element';
 
@@ -108,6 +109,12 @@ export class NgtxMultiElement<
   public last(): NgtxElement<Html, Component> {
     const itemCount = this.elements.length;
     return this.elements[itemCount - 1] ?? null;
+  }
+
+  public read<Type>(type: Token<Type>): NgtxMultiElement<Html, Type> {
+    return new NgtxMultiElement(
+      this.elements.map((element) => element.read(type)),
+    );
   }
 
   /**
