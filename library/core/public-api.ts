8a1304390e6acda5dec62a0f9e7b93dc07f8321d
julian.lang <julian.lang@centigrade.de>
2025-07-27 15:40:33 +0200

    fix: export testing modules

--- library/core/public-api.ts
+++ library/core/public-api.ts
@@ -3,3 +3,4 @@ export { asBool, asNumber } from './converter';
 export { NgtxElement } from './element';
 export { NgtxFixture } from './fixture';
 export { NgtxMultiElement } from './multi-element';
+export { TestingModule, provideForTesting } from './testing-modules';
