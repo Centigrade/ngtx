509162882491b0a869376f90e0311743b45a2643
julian.lang <julian.lang@centigrade.de>
2026-01-30 11:34:24 +0100

    fix: do unwrap state-def value before setting on target

--- library/utility/signals.ts
+++ library/utility/signals.ts
@@ -1,6 +1,6 @@
 import { isSignal, Signal, untracked, WritableSignal } from '@angular/core';
 import { ComponentFixture } from '@angular/core/testing';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { StateWithUnwrappedSignals } from '../types';
 
 export function valueOf<T, A extends Signal<T>[]>(value: T): T[];
@@ -10,6 +10,9 @@ export function valueOf<T, A extends Signal<T>[]>(
 ): T | T[] {
   if (Array.isArray(value)) {
     return value.map((v) => valueOf(v)) as T[];
+  }
+  if (isBehaviorSubject(value)) {
+    return value.value;
   }
 
   return isSignal(value) ? untracked(() => value()) : value;
@@ -21,18 +24,32 @@ export function setValue<T, K extends keyof T & string>(
   value: StateWithUnwrappedSignals<T>[K],
   fixture?: ComponentFixture<any>,
 ) {
+  const unwrappedValue = valueOf(value);
+
   if (isSignal(object[property])) {
-    if (isWritableSignal(object[property])) object[property].set(value);
-    else if (fixture) fixture.componentRef.setInput(property, value);
+    if (isWritableSignal(object[property]))
+      object[property].set(unwrappedValue);
+    else if (fixture) fixture.componentRef.setInput(property, unwrappedValue);
+    else
+      console.warn(
+        '[ngtx] cannot set value',
+        unwrappedValue,
+        `on non-writable signal "${property}:`,
+        object[property],
+      );
   } else if (isSubject(object[property])) {
-    object[property].next(value);
+    object[property].next(unwrappedValue);
   } else {
-    (object as any)[property] = value;
+    (object as any)[property] = unwrappedValue;
   }
 }
 
 function isSubject(value: unknown): value is Subject<any> {
   return typeof value === 'object' && value !== null && 'next' in value;
+}
+
+function isBehaviorSubject(value: unknown): value is BehaviorSubject<any> {
+  return isSubject(value) && 'value' in value;
 }
 
 export function isWritableSignal(
